c4d7a2e187ed359909686fa1d7e698bc012fe267
noahcharris <noahharris@deepsprings.edu>
2014-09-06 17:58:44 -0700

    removed groups and subgroups (except for group creation like usual) and added new topic methods with filters

--- server/postgres.js
+++ server/postgres.js
@@ -61,6 +61,71 @@ var query = client.query("SELECT * FROM groups "
 
 
 
+
+
+
+
+/***************************/
+/***  RETRIEVAL METHODS  ***/
+/***************************/
+
+
+//new topic retrieval methods with filters
+
+module.exports.retrieveTopTopics = function() {
+
+};
+
+module.exports.retrieveNewTopics = function() {
+
+};
+
+module.exports.retrieveHotTopics = function() {
+
+};
+
+
+
+module.exports.retrieveTopics = function(location, cb) {
+
+  client.query("SELECT * FROM topics WHERE location = $1;",[location], function(err, result) {
+    if (err) {
+      console.log('failed to retrieve topics for '+ location);
+    }
+     if (result) {
+      //console.log('retrieved topics for '+city+':', result.rows);
+      cb(result.rows);
+     }
+  });
+};
+
+
+module.exports.retrievePlace = function(location, cb) {
+  client.query("SELECT * FROM places WHERE location=$1;", [location], function(err, result) {
+      if (err) {
+        console.log(err);
+        console.log('error selecting from places');
+      } else {
+        cb(result.rows);
+      }
+  });
+};
+
+module.exports.retrieveUser = function(username, cb) {
+  client.query("SELECT * FROM users "
+    +"WHERE username=$1", [username], function(err, result) {
+      if (err) {
+        console.log(err);
+        console.log('error selecting from users');
+      } else {
+        cb(result.rows);
+      }
+    });
+};
+
+
+
+
 /**************************/
 /***  CREATION METHODS  ***/
 /**************************/
@@ -97,37 +162,6 @@ module.exports.createTopic = function(headline, link, contents, location, cb) {
 };
 
 
-//I do not need three different routes for topics, there must be a better way..
-
-
-module.exports.createGroupTopic = function(headline, link, contents, location, group, cb) {
-  client.query("INSERT INTO topics (headline, link, contents, location, agroup, rank) "
-    +"VALUES ($1, $2, $3, $4, $5, "+0+");",
-    [headline, link, contents, location, group], function(err, result) {
-      if (err) {
-        console.log('error inserting post into topics');
-        console.log(err);
-        cb(false);
-      } else {
-        cb(true);
-      }
-    });
-
-};
-
-module.exports.createSubgroupTopic = function(headline, link, contents, location, group, subgroup, cb) {
-  client.query("INSERT INTO topics (headline, link, contents, location, agroup, subgroup, rank) "
-    +"VALUES ($1, $2, $3, $4, $5, $6, "+0+");",
-    [headline, link, contents, location, group, subgroup], function(err, result) {
-      if (err) {
-        console.log('error inserting post into topics');
-        cb(false);
-      } else {
-        console.log(result);
-        cb(true);
-      }
-    });
-};
 
 
 module.exports.createComment = function(location, group, topic, headline, content, cb) {
@@ -197,128 +231,6 @@ module.exports.createUser = function(username, passhash, salt, origin, cb) {
 
 
 
-
-
-
-
-
-/***************************/
-/***  RETRIEVAL METHODS  ***/
-/***************************/
-
-
-module.exports.retrieveTopics = function(location, cb) {
-
-  client.query("SELECT * FROM topics WHERE location = $1;",[location], function(err, result) {
-    if (err) {
-      console.log('failed to retrieve topics for '+ location);
-    }
-     if (result) {
-      //console.log('retrieved topics for '+city+':', result.rows);
-      cb(result.rows);
-     }
-  });
-};
-
-module.exports.retrieveGroupTopics = function(location, group, cb) {
-  client.query("SELECT * FROM topics WHERE location=$1 "
-    +"AND agroup=$2;", [location, group], function(err, result) {
-      if (err) {
-        console.log('error retrieving group topics from database');
-      } else {
-        cb(result.rows);
-      }
-    });
-};
-
-module.exports.retrieveSubgroupTopics = function(location, group, name, cb) {
-  //TODO
-  client.query("SELECT * FROM topics WHERE location=$1 AND "
-    +"agroup=$2 AND subgroup=$3;", [location, group, name], function(err, result) {
-      if (err) {
-        console.log('error retrieving retrieving subgroup topics from database');
-      } else {
-        cb(result.rows);
-      }
-    });
-
-};
-
-module.exports.retrieveGroups = function(location, cb) {
-  client.query("SELECT * FROM groups WHERE location=$1;", [location], function(err, result) {
-    if (err) {
-      console.log('error retrieving from groups');
-    } else {
-      cb(result.rows);
-    }
-  });
-};
-
-module.exports.retrieveSubgroups = function(location, group, cb) {
-  client.query("SELECT * FROM subgroups WHERE "
-    +"location=$1 AND agroup=$2;", [location, group], function(err, result) {
-      if (err) {
-        console.log('error selecting from subgroups');
-      } else {
-        cb(result.rows);
-      }
-    });
-};
-
-module.exports.retrievePlace = function(location, cb) {
-  client.query("SELECT * FROM places WHERE location=$1;", [location], function(err, result) {
-      if (err) {
-        console.log(err);
-        console.log('error selecting from places');
-      } else {
-        cb(result.rows);
-      }
-  });
-};
-
-module.exports.retrieveUser = function(username, cb) {
-  client.query("SELECT * FROM users "
-    +"WHERE username=$1", [username], function(err, result) {
-      if (err) {
-        console.log(err);
-        console.log('error selecting from users');
-      } else {
-        cb(result.rows);
-      }
-    });
-};
-
-
-
-
-//for checking existence of group
-module.exports.getGroupForPath = function(location, group, cb) {
-  console.log('querying for group, location:', location, ' group:', group);
-  client.query("SELECT * FROM groups WHERE "
-    +"location=$1 AND name=$2;", [location, group], function(err, result) {
-      if (err) {
-        console.log('error selecting group for path');
-      } else {
-        cb(result.rows);
-      }
-    });
-
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 module.exports.createTables = function() {
 
   //NEED TO ADD TIMESTAMPS TO ALL THESE (need to research postgres+time)
@@ -353,18 +265,6 @@ module.exports.createTables = function() {
       } else {
         console.log('votes table created');
       }
-  });
-
-  client.query("CREATE TABLE bookmarks ( "
-    +"id         SERIAL PRIMARY KEY,"
-    +"username   VARCHAR(50),"
-    +"name       VARCHAR(50),"
-    +"url        VARCHAR(200));", function(err, result) {
-    if (err) {
-      console.log('failed to create bookmarks table');
-    } else {
-      console.log('bookmarks table created');
-    }
   });
 
 
@@ -473,29 +373,6 @@ module.exports.createTables = function() {
     }
   });
 
-  client.query("CREATE TABLE subgroups ("
-    +"id             SERIAL PRIMARY KEY,"
-    +"name           VARCHAR(50),"
-    +"description    TEXT,"
-    +"creator        VARCHAR(50),"
-    +"size           INTEGER,"
-    +"open           BOOLEAN,"
-    +"location       VARCHAR(100),"
-    +"country        VARCHAR(50),"
-    +"state          VARCHAR(50),"
-    +"city           VARCHAR(50),"
-    //have to use this bullshit variable because group is a keyword
-    +"agroup         VARCHAR(100),"
-    +"rank           INTEGER);", function(err, result) {
-      if (err) {
-        console.log('failed to create subgroups table');
-        console.log(result);
-      } else {
-        console.log('subgroups table created');
-      }
-  });
-
-
   client.query("CREATE TABLE places ("
     +"id            SERIAL PRIMARY KEY,"
     +"location      VARCHAR(100),"
@@ -532,37 +409,6 @@ module.exports.createTables = function() {
 
 };
 
-// module.exports.populateGroups = function() {
-//   client.query("INSERT INTO groups "
-//     +"(geom, name, description, size, rank, public,"
-//     +"open, latitude, longitude, location) "
-//     //inserting using postGIS function
-//     //postGIS order is longitude, latitude!!
-//     +"VALUES (ST_PointFromText('POINT(2.330 48.864)', 4269), 'Silhouette',"
-//     +"'Amidst the catacombs.', 311, 4072, true, true, 48.864, 2.330, 'France/Paris');", function(err, result) {
-//       if (err) {
-//         console.log('error inserting into groups table');
-//       } else {
-//         console.log('successfully inserted mock points data');
-//       }
-//   });
-// };
-
-// module.exports.populateSubgroups = function() {
-//   client.query("INSERT INTO subgroups "
-//     +"(name, agroup, description, size, rank, "
-//     +"open, location) "
-//     //inserting using postGIS function
-//     //postGIS order is longitude, latitude!!
-//     +"VALUES ('Echo 9', 'Silhouette',"
-//     +"'Amidst the catacombs.', 27, 56, true, 'France/Paris');", function(err, result) {
-//       if (err) {
-//         console.log('error inserting into subgroups table');
-//       } else {
-//         console.log('successfully inserted subgroup data');
-//       }
-//   });
-// };
 
 
 // module.exports.populatePlaces = function() {
