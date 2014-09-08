02f46becaac8626adfcd540baabdb40892d213b8
noahcharris <noahharris@deepsprings.edu>
2014-09-07 21:32:52 -0700

    just set the click handlers in the render instead of using setHandlers, in fact, I should just minimize the setHandlers shit

--- client/views/ChannelView.js
+++ client/views/ChannelView.js
@@ -23,16 +23,20 @@ Agora.Views.ChannelView = Backbone.View.extend({
 
     var channel = this.app.get('channel');
     console.log('rendering channelView with channel: ', channel);
-    this.$el.append($('<span class="channelName">&nbsp&nbspChannel:&nbsp<strong>' + channel + '</strong>&nbsp</span>'));
+    this.$el.append($('<span class="channelName">&nbsp&nbspChannel:&nbsp</span>')); //<strong>' + channel + '</strong>&nbsp</span>'));
+    var searchButton = $('<img id="searchChannelButton" src="/resources/images/search.png" width="10px" height="10px"></img>');
 
-  },
-
-  setHandlers: function() {
-
-    var that = this;
-    $('.channelView').on('click', function(e) {
+    searchButton.on('click', function() { 
       that.$el.empty();
-      that.$el.append($('<span class="channelName">&nbsp&nbspChannel:&nbsp<input id="channelInput"></input></span>'));
+      that.$el.append($('<span class="channelName">&nbsp&nbspChannel:&nbsp</span>'));
+
+      var searchButton = $('<img id="searchChannelButton" src="/resources/images/search.png" width="10px" height="10px"></img>');
+
+      searchButton.on('click', function() { alert('mah dick'); });
+      that.$el.append(searchButton);
+
+      that.$el.append($('<input id="channelInput"></input>'));
+
       $('#channelInput').focus();
       $('#channelInput').focusout(function() {
         //REMEMBER TO CALL BOTH RENDER AND SETHANDLERS
@@ -45,8 +49,40 @@ Agora.Views.ChannelView = Backbone.View.extend({
         console.log($('#channelInput').val());
         //AJAX CALL FOR SEARCH SUGGESTIONS
         //throttle the ajax call here
-      });
+       });
     });
+    this.$el.append(searchButton);
+  },
+
+  setHandlers: function() {
+
+    var that = this;
+    console.log('#searchChannelButton');
+    // $('.channelView').on('click', function(e) {
+    //   that.$el.empty();
+    //   that.$el.append($('<span class="channelName">&nbsp&nbspChannel:&nbsp</span>'));
+
+    //   var searchButton = $('<img id="searchChannelButton" src="/resources/images/search.png" width="10px" height="10px"></img>');
+
+    //   searchButton.on('click', function() { alert('mah dick'); });
+    //   that.$el.append(searchButton);
+
+    //   that.$el.append($('<input id="channelInput"></input>'));
+
+    //   $('#channelInput').focus();
+    //   $('#channelInput').focusout(function() {
+    //     //REMEMBER TO CALL BOTH RENDER AND SETHANDLERS
+    //     $('.channelView').empty();
+    //     that.render();
+    //     that.setHandlers();
+    //   });
+    //   //keyup is the best way to get all the keys, not ideal
+    //   $('#channelInput').on('keyup', function(e) {
+    //     console.log($('#channelInput').val());
+    //     //AJAX CALL FOR SEARCH SUGGESTIONS
+    //     //throttle the ajax call here
+    //   });
+    // });
 
   }
 
