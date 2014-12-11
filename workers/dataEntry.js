4873c2ba5e7d6b53e9ddbbc98bd59ad289717dcd
noahcharris <noahharris@deepsprings.edu>
2014-12-10 18:14:27 -0800

    dataEntry script working basically

--- workers/dataEntry.js
+++ workers/dataEntry.js
@@ -1,3 +1,11 @@
+var pg = require('pg');
+
+var conString = 'postgres://noahharris:mypassword@agora2db.cfm6lqsulycg.us-west-2.rds.amazonaws.com:5432/thebestdb';
+//var conString = 'postgres://noahharris@localhost:5432/noahharris';
+
+var client = new pg.Client(conString);
+client.connect();
+
 var countryData = require('../client/resources/newCountries.js');
 var cityData = require('../client/resources/cities.js');
 var statesData = require('../client/resources/us-states.js');
@@ -8,14 +16,31 @@ states = statesData.states.features;
 
 
 //INSERT WORLD
+client.query("INSERT INTO locations (type, isusercreated, name, population, public) "
+  +"VALUES ('Location', false, $1, 0, true);",
+  ['World'],
+  function(err, result) {
+    if (err) {
+      console.log('error inserting into locations: ', err);
+    } else {
 
-
+    }
+});
 
 
 //INSERT COUNTRIES
 for (var i=0; i < countries.length ;i++) {
 
-  console.log('insert ', countries[i].properties.name, ' into database');
+  client.query("INSERT INTO locations (type, isusercreated, name, population, public) "
+    +"VALUES ('Location', false, $1, 0, true);",
+    [countries[i].properties.name],
+    function(err, result) {
+      if (err) {
+        console.log('error inserting into locations: ', err);
+      } else {
+
+      }
+  });
 
 };
 
@@ -23,7 +48,16 @@ for (var i=0; i < countries.length ;i++) {
 //INSERT CITIES
 for (var i=0; i < cities.length ;i++) {
 
-  console.log('insert ', cities[i].properties.city, ' into database');
+  client.query("INSERT INTO locations (type, isusercreated, name, population, public) "
+    +"VALUES ('Location', false, $1, 0, true);",
+    [cities[i].properties.city],
+    function(err, result) {
+      if (err) {
+        console.log('error inserting into locations: ', err);
+      } else {
+
+      }
+  });
 
 };
 
@@ -31,6 +65,29 @@ for (var i=0; i < cities.length ;i++) {
 //INSERT STATES
 for (var i=0; i < states.length ;i++) {
 
-  console.log('insert ', states[i].properties.name, ' into database');
+  client.query("INSERT INTO locations (type, isusercreated, name, population, public) "
+    +"VALUES ('Location', false, $1, 0, true);",
+    [states[i].properties.name],
+    function(err, result) {
+      if (err) {
+        console.log('error inserting into locations: ', err);
+      } else {
+
+      }
+  });
 
 };
+
+
+
+
+
+//CHANNELS
+
+
+
+
+
+
+
+
